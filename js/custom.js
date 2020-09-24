66877bdfa37010a0033263c8f2c4ce92fedd1da7
Anna Qi <annaqi13@gmail.com>
2020-09-23 22:42:42 -0700

    adding about and projects pages

--- js/custom.js
+++ js/custom.js
@@ -10,15 +10,17 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > scrollTo || document.documentElement.scrollTop > scrollTo) {
     for (var i = 0; i < allNavLinks.length; i++) {
-      allNavLinks[i].style.color = primaryColor;
+      allNavLinks[i].classList.remove("nav-light")
+      allNavLinks[i].classList.add("nav-dark")
     }
     resumeButton.classList.remove("btn-light")
-    resumeButton.classList.add("btn-primary")
+    resumeButton.classList.add("btn-custom")
   } else {
     for (var i = 0; i < allNavLinks.length; i++) {
-      allNavLinks[i].style.color = secondaryColor;
+      allNavLinks[i].classList.remove("nav-dark")
+      allNavLinks[i].classList.add("nav-light")
     }
-    resumeButton.classList.remove("btn-primary")
+    resumeButton.classList.remove("btn-custom")
     resumeButton.classList.add("btn-light")
   }
 }
